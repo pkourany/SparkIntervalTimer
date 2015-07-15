aaf87f246e8098e1a1ca3c6e7353e9592c7d243e
Paul Kourany <pkourany@users.noreply.github.com>
2015-07-15 12:23:25 -0400

    Update SparkIntervalTimer.h

--- firmware/SparkIntervalTimer.h
+++ firmware/SparkIntervalTimer.h
@@ -25,33 +25,53 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #include "application.h"
 
+#if defined(PLATFORM_ID)
+#if (PLATFORM_ID == 0) // Core
+  #define SYSCORECLOCK	72000000UL		// Core provides SYSCORECLOCK to all timers
+#elif (PLATFORM_ID == 6) // Photon
+  #define SYSCORECLOCK	60000000UL		// Photon provides SYSCORECLOCK/2 as root clock to timers (except TIMER1)
+#else
+  #error "*** PLATFORM_ID not supported by this library. PLATFORM should be Core or Photon ***"
+#endif
+#endif
 
 enum {uSec, hmSec};			// microseconds or half-milliseconds
+enum action {INT_DISABLE, INT_ENABLE};
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern void (*Wiring_TIM2_Interrupt_Handler)(void);
-extern void (*Wiring_TIM3_Interrupt_Handler)(void);
-extern void (*Wiring_TIM4_Interrupt_Handler)(void);
 
+#if (PLATFORM_ID == 0)	//Core
 extern void Wiring_TIM2_Interrupt_Handler_override(void);
 extern void Wiring_TIM3_Interrupt_Handler_override(void);
 extern void Wiring_TIM4_Interrupt_Handler_override(void);
 
-enum action {INT_DISABLE, INT_ENABLE};
 enum TIMid {TIMER2, TIMER3, TIMER4, AUTO=255};
+#elif (PLATFORM_ID == 6)	//Photon
+extern void Wiring_TIM3_Interrupt_Handler_override(void);
+extern void Wiring_TIM4_Interrupt_Handler_override(void);
+extern void Wiring_TIM5_Interrupt_Handler_override(void);
+extern void Wiring_TIM6_Interrupt_Handler_override(void);
+extern void Wiring_TIM7_Interrupt_Handler_override(void);
+
+enum TIMid {TIMER3, TIMER4, TIMER5, TIMER6, TIMER7, AUTO=255};
+#endif
 
 class IntervalTimer {
   private:
 	typedef void (*ISRcallback)();
     enum {TIMER_OFF, TIMER_SIT};
+#if (PLATFORM_ID == 0)	//Core
     static const uint8_t NUM_SIT = 3;
+#elif (PLATFORM_ID == 6)	//Photon
+    static const uint8_t NUM_SIT = 5;
+#endif
 	
 	// Timer ClockDivision = DIV4
-	const uint16_t SIT_PRESCALERu = (uint16_t)(SystemCoreClock / 1000000) - 1;	//To get TIM counter clock = 1MHz
-	const uint16_t SIT_PRESCALERm = (uint16_t)(SystemCoreClock / 2000) - 1;		//To get TIM counter clock = 2KHz
+	const uint16_t SIT_PRESCALERu = (uint16_t)(SYSCORECLOCK / 1000000UL) - 1;	//To get TIM counter clock = 1MHz
+	const uint16_t SIT_PRESCALERm = (uint16_t)(SYSCORECLOCK / 2000UL) - 1;		//To get TIM counter clock = 2KHz
     const uint16_t MAX_PERIOD = UINT16_MAX;		// 1-65535 us
 	
     static bool SIT_used[NUM_SIT];
@@ -65,12 +85,64 @@ class IntervalTimer {
     bool beginCycles(void (*isrCallback)(), uint16_t Period, bool scale, TIMid id);
 	
   public:
-    IntervalTimer() { 
-        status = TIMER_OFF; 
-        Wiring_TIM2_Interrupt_Handler = Wiring_TIM2_Interrupt_Handler_override;
-        Wiring_TIM3_Interrupt_Handler = Wiring_TIM3_Interrupt_Handler_override;
-        Wiring_TIM4_Interrupt_Handler = Wiring_TIM4_Interrupt_Handler_override;
+    IntervalTimer() {
+	status = TIMER_OFF;
+	
+	for (int i=0; i < NUM_SIT; i++)		//Set all SIT slots to unused
+		SIT_used[i] = false;
+	
+	// Attach timer interrupt handlers
+#if defined (STM32F10X_MD)	//Core
+	if (!attachSystemInterrupt(SysInterrupt_TIM2_Update, Wiring_TIM2_Interrupt_Handler_override)) ;	//error
+	if (!attachSystemInterrupt(SysInterrupt_TIM3_Update, Wiring_TIM3_Interrupt_Handler_override)) ;	//error
+	if (!attachSystemInterrupt(SysInterrupt_TIM4_Update, Wiring_TIM4_Interrupt_Handler_override)) ;	//error
+#elif defined (STM32F2XX)	//Photon+
+	if (!attachSystemInterrupt(SysInterrupt_TIM3_Update, Wiring_TIM3_Interrupt_Handler_override)) ;	//error
+	if (!attachSystemInterrupt(SysInterrupt_TIM4_Update, Wiring_TIM4_Interrupt_Handler_override)) ;	//error
+	if (!attachSystemInterrupt(SysInterrupt_TIM5_Update, Wiring_TIM5_Interrupt_Handler_override)) ;	//error
+	if (!attachSystemInterrupt(SysInterrupt_TIM6_Update, Wiring_TIM6_Interrupt_Handler_override)) digitalWrite(D7,HIGH);	//error
+	if (!attachSystemInterrupt(SysInterrupt_TIM7_Update, Wiring_TIM7_Interrupt_Handler_override)) digitalWrite(D5,HIGH);	//error
+#endif
+
+/*
+    SysInterrupt_TIM1_BRK_TIM9_IRQ,
+    SysInterrupt_TIM1_Break,
+    SysInterrupt_TIM9_Compare1,
+    SysInterrupt_TIM9_Compare2,
+    SysInterrupt_TIM9_Update,
+    SysInterrupt_TIM9_Trigger,
+    SysInterrupt_TIM1_UP_TIM10_IRQ,
+    SysInterrupt_TIM1_Update,
+    SysInterrupt_TIM10_Compare,
+    SysInterrupt_TIM10_Update,
+    SysInterrupt_TIM8_BRK_TIM12_IRQ,
+    SysInterrupt_TIM8_Break,
+    SysInterrupt_TIM12_Compare1,
+    SysInterrupt_TIM12_Compare2,
+    SysInterrupt_TIM12_Update,
+    SysInterrupt_TIM12_Trigger,
+    SysInterrupt_TIM8_UP_TIM13_IRQ,
+    SysInterrupt_TIM8_Update,
+    SysInterrupt_TIM13_Compare,
+    SysInterrupt_TIM13_Update,
+    SysInterrupt_TIM8_TRG_COM_TIM14_IRQ,
+    SysInterrupt_TIM8_Trigger,
+    SysInterrupt_TIM14_COM,
+    SysInterrupt_TIM14_Compare,
+    SysInterrupt_TIM14_Update,
+    SysInterrupt_TIM8_IRQ,
+    SysInterrupt_TIM8_Compare1,
+    SysInterrupt_TIM8_Compare2,
+    SysInterrupt_TIM8_Compare3,
+    SysInterrupt_TIM1_TRG_COM_TIM11_IRQ,
+    SysInterrupt_TIM1_Trigger,
+    SysInterrupt_TIM1_COM,
+    SysInterrupt_TIM11_Compare,
+    SysInterrupt_TIM11_Update,
+*/
+
     }
+	
     ~IntervalTimer() { end(); }
 	
     bool begin(void (*isrCallback)(), uint16_t Period, bool scale) {
